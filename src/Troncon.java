5a2523545216eb8d2121700e6dd5ebbbc786a52a
Olivier DE VOS <olivier.devos@student.vinci.be>
2020-03-06 09:19:29 +0100

    classes de base

--- src/Troncon.java
+++ src/Troncon.java
@@ -6,4 +6,25 @@ public class Troncon {
 	private Stop arrivee;
 	private int duree;
 	
+	public Troncon(Ligne ligne, Stop depart, Stop arrivee, int duree) {
+		super();
+		this.ligne = ligne;
+		this.depart = depart;
+		this.arrivee = arrivee;
+		this.duree = duree;
+	}
+	public Ligne getLigne() {
+		return ligne;
+	}
+	public Stop getDepart() {
+		return depart;
+	}
+	public Stop getArrivee() {
+		return arrivee;
+	}
+	public int getDuree() {
+		return duree;
+	}
+	
+	
 }
