4a6df1d8b91f6d1695dff9277c1f54191efe2e30
Olivier DE VOS <olivier.devos@student.vinci.be>
2020-03-06 09:24:24 +0100

    classes de bases complete

--- src/Ligne.java
+++ src/Ligne.java
@@ -7,4 +7,27 @@ public class Ligne {
 	private String type;
 	private int attenteMoyenne;
 	
+	public Ligne(String nom, String source, String destination, String type, int attenteMoyenne) {
+		super();
+		this.nom = nom;
+		this.source = source;
+		this.destination = destination;
+		this.type = type;
+		this.attenteMoyenne = attenteMoyenne;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public String getSource() {
+		return source;
+	}
+	public String getDestination() {
+		return destination;
+	}
+	public String getType() {
+		return type;
+	}
+	public int getAttenteMoyenne() {
+		return attenteMoyenne;
+	}
 }
--- src/Stop.java
+++ src/Stop.java
@@ -1,8 +1,25 @@
 import java.util.Map;
+import java.util.Set;
 
 public class Stop {
 
 	private String nom;
-	private Map<Stop, String> stops;
+	private Map<Stop, String> stations;
+	private Map<String, Set<Stop>> stops;
 	
+	public Stop(String nom, Map<Stop, String> stations, Map<String, Set<Stop>> stops) {
+		super();
+		this.nom = nom;
+		this.stations = stations;
+		this.stops = stops;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public Map<Stop, String> getStations() {
+		return stations;
+	}
+	public Map<String, Set<Stop>> getStops() {
+		return stops;
+	}
 }
