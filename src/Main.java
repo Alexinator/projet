addec084eae96476772fd9df99bdc94b0c2430ff
Alexandre Chapelle <alexchap98@hotmail.com>
2020-03-20 10:49:13 +0100

    preparation

--- src/Main.java
+++ src/Main.java
@@ -16,19 +16,19 @@ import org.w3c.dom.Document;
 public class Main {
 	public static void main(String[] args) {
 		try {
-			//File inputFile = new File("stib.xml");
-			File inputFile = new File("stibattente0.xml");
+			File inputFile = new File("stib.xml");
+			//File inputFile = new File("stibattente0.xml");
 			SAXParserFactory factory = SAXParserFactory.newInstance();
 			SAXParser saxParser = factory.newSAXParser();
 			SAXHandler userhandler = new SAXHandler();
 			saxParser.parse(inputFile, userhandler);
 			Graph g = userhandler.getGraph();
-			//g.calculerCheminMinimisantNombreTroncons("MALIBRAN", "ALMA", "output.xml");
+			g.calculerCheminMinimisantNombreTroncons("MALIBRAN", "ALMA", "output.xml");
 			g.calculerCheminMinimisantTempsTransport("MALIBRAN", "ALMA", "output2.xml");
 
 			//BONUS : DOMParser
-			//File xmlFile = new File("stib.xml");
-			File xmlFile = new File("stibattente0.xml");
+			File xmlFile = new File("stib.xml");
+			//File xmlFile = new File("stibattente0.xml");
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 			Document doc = dBuilder.parse(xmlFile);
